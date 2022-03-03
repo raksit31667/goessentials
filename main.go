beab17673de8cf69361ec9bd98eb7743df678909
raksit31667 <raksit.m@ku.th>
2022-03-03 18:14:21 +0700

    :beers: learn Go functions

--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
+func say() string {
+	return "Hello, Gopher"
+}
+
+func say2(s string) (string, int) {
+	return s + " Hello, Gopher", 2
+}
+
 func main() {
 	s := "Hello, Gopher"
 	var age int
 	fmt.Println(s)
 	fmt.Println(age)
+
+	h := say()
+	fmt.Println(h)
+
+	ha, hb := say2("foo")
+	fmt.Println(ha, hb)
 }
